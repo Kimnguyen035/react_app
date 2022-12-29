e20bee02177c154750f3ff36e06873ad9ecd4c83
kimnt <you@example.com>
2022-12-29 09:41:29 +0700

    setup project and init home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 import './css/style.css'
 import Navbar from './components/Navbar/Navbar'
-
-
+import Home from './page/home/Home'
 
 function App() {
   return (
     <main>
         <Navbar />
+        <Home />
     </main>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import { slide as Menu } from 'react-burger-menu'
+import '../../css/navbar.css'
 
 const Navbar = () => {
 
