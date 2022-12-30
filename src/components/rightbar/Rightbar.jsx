3481278ef3484ddc830ed059cf29b61bd780e43a
kimnt <you@example.com>
2022-12-30 16:24:47 +0700

    face data for rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,10 +1,27 @@
 import './rightbar.css'
+import { Users } from '../../dummyData'
+import Online from '../online/Online'
 
 const Rightbar = () => {
 
     return (
         <div className='rightbar'>
-            This is rightbar.
+            <div className='rightbarWrapper'>
+                <div className='birthdayContainer'>
+                    <img src='/assets/f.gif' alt='' className='birthdayImg' />
+                    <span className='birthdayText'>
+                        {''}
+                        Polo Foster and 3 other friends have a birthday today
+                    </span>
+                </div>
+            </div>
+            <img src='/assets/ad.png' alt='' className='rightbarAd' />
+            <h4 className='rightbarTitle'>Online Friends</h4>
+            <ul className='rightbarFriendList'>
+                {Users.map((u) => (
+                    <Online key={u.id} user={u} />
+                ))}
+            </ul>
         </div>
     )
 }
